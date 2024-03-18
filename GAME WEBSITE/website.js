a4f1daadbf808cf2bc088db9b4bb1f26c4a53e88
BOTTZX <sujaybhakri21@gmail.com>
2024-03-18 22:31:40 +0530

    18th march final

--- GAME WEBSITE/website.js
+++ GAME WEBSITE/website.js
@@ -3,3 +3,11 @@ document.getElementById("tic-tac-toe").addEventListener("click", redirectPage);
 function redirectPage() {
   window.location.replace("tic-tac-toe.html");
 }
+
+document
+  .getElementById("dots-and-boxes")
+  .addEventListener("click", redirectPage);
+
+function redirectPage() {
+  window.location.replace("dots-and-boxes.html");
+}
